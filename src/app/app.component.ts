f9fab61fe09d3533b613ebd4aad0676af45d5e79
Ordna-dev <ordnajelaorerreug@gmail.com>
2024-01-28 21:49:43 -0600

    Dashboards y perfil completados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router, NavigationEnd, Event } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { ellipseOutline, logoGoogle, accessibilitySharp, logInSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { pencilSharp, ellipseOutline, logoGoogle, accessibilitySharp, logInSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -31,7 +31,7 @@ export class AppComponent {
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(private router: Router) {
-    addIcons({ ellipseOutline, logoGoogle, accessibilitySharp, logInSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ pencilSharp, ellipseOutline, logoGoogle, accessibilitySharp, logInSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
 
   //AGM 26/01/2024. BUG: Al iniciar la aplicación se muestra el slidemenu cuando no debería, al refrescar ya esta todo normal
