d0f2d0a497fffc7decc84bfe7c110b6703065bd2
Ordna-dev <ordnajelaorerreug@gmail.com>
2024-02-01 12:57:32 -0600

    Todo el contenido del dashboard del alumno comentado y ordenado

--- src/app/dashboard-alumno/dashboard-alumno.page.ts
+++ src/app/dashboard-alumno/dashboard-alumno.page.ts
@@ -15,35 +15,30 @@ export class DashboardAlumnoPage implements OnInit {
 
   constructor(private router: Router) { }
 
+  // AGM 31/01/2024 - Redireccionamiento al grupo
   irGrupo() {
     // Futuras acciones
     this.router.navigate(['/grupo-alumno']);
   }
 
-  cerrarSesion() {
+  // AGM 31/01/2024 - Redireccionamiento al cierre de sesion
+  logOut() {
     // Acciones como limpiar datos de sesión
     this.router.navigate(['/login']);
   }
 
-  isFifthModalOpen = false;
-
-  //AGM 31/01/2024 - Abrir o cerrar el quinto modal
-  setFifthOpen(isOpen: boolean) {
-    this.isFifthModalOpen = isOpen; 
-  }
-
-  //AGM 30/01/2024 - Pintar de colores aleatorios las cards
-  /*colores = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'light', 'medium', 'dark'];
-
-  getColorAleatorio(): string {
-    const indiceAleatorio = Math.floor(Math.random() * this.colores.length);
-    return this.colores[indiceAleatorio];
-  }*/
-
+  // AGM 31/01/2024 - Abrir o cerrar el primer modal
   isModalOpen = false;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
+  }
+
+  // AGM 31/01/2024 - Abrir o cerrar el quinto modal
+  isFifthModalOpen = false;
+
+  setFifthOpen(isOpen: boolean) {
+    this.isFifthModalOpen = isOpen; 
   }
 
   ngOnInit() {
