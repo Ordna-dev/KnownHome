353788e8d75e8aa168f80ad3e88f3fedc942d07e
Ordna-dev <ordnajelaorerreug@gmail.com>
2024-02-05 23:36:45 -0600

    Primer paso de integracion completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterLink, RouterLinkActive, Router, NavigationEnd, Event } from '@ang
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { images, camera, chevronForwardOutline, trash, accessibility, peopleOutline, closeOutline, arrowBackOutline, helpCircleOutline, documentText, pencilSharp, ellipseOutline, logoGoogle, accessibilitySharp, logInSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet],
+  imports: [HttpClientModule, RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet],
 })
 export class AppComponent {
   public appPages = [
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -13,14 +13,63 @@ import { Router } from '@angular/router';
 })
 
 export class LoginPage implements OnInit {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
   constructor(private router: Router) {} 
 
   ngOnInit() {
+    
+  }
+
+  getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
+
+  testLogout() {
+    fetch('http://localhost:5000/auth/logout')
+      .then(response => response.text()) // Obtener respuesta como texto
+      .then(html => {
+        console.log(html); // Mostrar la respuesta HTML en consola
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
   // AGM 28/01/2024 - Redireccionar al loguearse
   onLogin() {
-    this.router.navigate(['/dashboard-alumno']);  // Redirige a la página del dashboard del alumno - Por cambiar
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Se requieren ambos campos';
+      return;
+    }
+
+    // Realizar la solicitud PUT para el inicio de sesión
+    fetch('http://localhost:5000/auth/maestro/login', {
+      method: 'PUT',
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        this.errorMessage = data.message;
+      } else {
+        this.router.navigate(['/dashboard-maestro']); // Redirige al dashboard del maestro
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      this.errorMessage = 'Error al conectar con el servidor';
+    });
   }
+  
 }
